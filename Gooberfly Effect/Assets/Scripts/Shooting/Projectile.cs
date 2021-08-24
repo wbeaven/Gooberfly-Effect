bc12da55b42d06f87a7a9915a1302fee984a80f4
Andrew Phillip Gonzalez <Lekonia@iinet.net.au>
2021-08-24 13:38:27 +1000

    slight tweak on scripts

--- Gooberfly Effect/Assets/Scripts/Shooting/Projectile.cs
+++ Gooberfly Effect/Assets/Scripts/Shooting/Projectile.cs
@@ -14,11 +14,11 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("NotChaos"))
-        {
-            Chaos.chaosLevel += 1;
+        {            
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+            Chaos.chaosLevel += 1;
             //chaosController.GetComponent<Chaos>().chaosLevel += 1;
-            //ChaosScript.chaosLevel += 1;
+            //ChaosScript.chaosLevel += 1; //Not sure if the Script part is necessary anymore, that may have been deprecated
             print("ADDING CHAOS");
         }
     }
