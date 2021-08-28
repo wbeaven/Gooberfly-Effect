577a5c0fa5d3683861189c5f5c03d57cdcc41862
Andrew Phillip Gonzalez <Lekonia@iinet.net.au>
2021-08-28 19:00:41 +1000

    it works!?!

--- Gooberfly Effect/Assets/Scripts/Experimental Scripts/Randomizer_V2.cs
+++ Gooberfly Effect/Assets/Scripts/Experimental Scripts/Randomizer_V2.cs
@@ -12,7 +12,9 @@ public class Randomizer_V2 : MonoBehaviour
     {
         int n = Random.Range(0, objectsToInstantiate.Length); //This may be the key to creating arrays and randomizing
 
-        var position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+        //var position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+        //var billyX = new Vector3(Random.Range(minX, maxX), 0, 0);
+        var position = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z) - new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
         GameObject g = Instantiate(objectsToInstantiate[n], position, objectsToInstantiate[n].transform.rotation);
     }
 }
